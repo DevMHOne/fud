6c9f93280be73233bfda66e17f07df1934611069
DevMHOne <110272440+DevMHOne@users.noreply.github.com>
2024-03-18 15:28:59 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'avatars3.githubusercontent.com'
+        hostname: 'fuds.pages.dev'
       }
     ]
   }
